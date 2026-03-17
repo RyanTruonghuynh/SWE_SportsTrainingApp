f0bba446a41766fc0c369ed91c9189e3fd24cd3b
Aryan Wagh <aryan.wagh05@gmail.com>
2026-03-17 01:11:05 -0500

    added comments to db.js, user.js, .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,19 @@
+// Import mongoose to interact with MongoDB
 const mongoose = require("mongoose");
 
+//establish connection to MongoDB
 const connectDB = async () => {
+//Attempt to connect using the connection string from .env
   try{
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   }
   catch (error){
     console.error("Database connection failed:", error.message);
+    //Exit the application to prevent running without Database
     process.exit(1);
   }
 };
 
+//Export the function so it can be used in server.js
 module.exports = connectDB;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,31 @@
+// Import mongoose to define schema and model
 const mongoose = require("mongoose");
+
 
 const userSchema = new mongoose.Schema(
   {
+    //the user's username, which is required and will be trimmed of whitespace
     username:{
       type: String,
       required: true,
       trim: true,
     },
+    //the user's email address, which must be unique and is required for account creation
     email:{
       type: String,
       required: true,
       unique: true,
       trim: true,
     },
+    //the user's password, which is required
     password:{
       type: String,
       required: true,
     },
   },
-  {timestamps: true}
+  {timestamps: true}    // automatically adds createdAt & updatedAt
 );
 
+
+//Export the model so it can be used in routes/controllers
 module.exports = mongoose.model("User", userSchema);
